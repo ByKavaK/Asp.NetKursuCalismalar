57960cf4fd6927274cdde6483315c790c9df64eb
Yasin Kavak <ysnkvk2002@gmail.com>
2024-02-01 14:52:04 +0300

    FormsApp başlangıç
    ilk 5 video

---
+++ FormsApp/FormsApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormsApp.Models
+{
+    public class Product
+    {
+        [Display(Name = "Urun Id")]
+        public int ProductId { get; set; }
+        [Display(Name = "Urun Adı")]
+        public string? Name { get; set; }
+        [Display(Name = "Fiyat")]
+        public decimal Price { get; set; }
+        [Display(Name = "Resim")]
+        public string? Image { get; set; }
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
