f6080672e10f2d0a5ae1f10da811a510c20d533f
Yasin Kavak <ysnkvk2002@gmail.com>
2024-03-08 15:57:08 +0300

    FormsApp
    3. 5 ders

--- FormsApp/FormsApp/Controllers/HomeController.cs
+++ FormsApp/FormsApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
+using System.IO;
 
 namespace FormsApp.Controllers
 {
@@ -19,7 +20,7 @@ namespace FormsApp.Controllers
             if (!String.IsNullOrEmpty(searchString))
             {
                 ViewBag.SearchString = searchString;
-                products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+                products = products.Where(p => p.Name!.ToLower().Contains(searchString)).ToList();
             }
 
             if (!String.IsNullOrEmpty(category) && category != "0")
@@ -46,10 +47,78 @@ namespace FormsApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product model)
+        public async Task<IActionResult> CreateAsync(Product model, IFormFile imageFile)
         {
-            Repository.CreateProduct(model);
-            return RedirectToAction("Index");
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+            var extension = Path.GetExtension(imageFile.FileName);
+            var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+
+            if(imageFile != null)
+            {
+                if(!allowedExtensions.Contains(extension))
+                {
+                    ModelState.AddModelError("", "Geçerli Bir Resim Seçiniz.");
+                }
+            }
+
+            if(ModelState.IsValid)
+            {
+                if(imageFile != null)
+                {
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(stream);
+                    }
+                }
+                model.Image = randomFileName;
+                model.ProductId = Repository.Products.Count + 1;
+                Repository.CreateProduct(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(model);
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var entity = Repository.Products.FirstOrDefault(p => p.ProductId == id);
+            if(entity == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(entity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Product model, IFormFile? imageFile)
+        {
+            if(id != model.ProductId)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                if (imageFile != null)
+                {
+                    var extension = Path.GetExtension(imageFile.FileName);
+                    var randomFileName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFileName);
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(stream);
+                    }
+                    model.Image = randomFileName;
+                }
+                Repository.EditProduct(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+            return View(model);
         }
     }
 }
--- FormsApp/FormsApp/obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
+++ FormsApp/FormsApp/obj/Debug/net7.0/FormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57960cf4fd6927274cdde6483315c790c9df64eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279e22a6d700e28170232b610c6efa956dfd891e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
