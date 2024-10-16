a1988aa4975361f97a101bb40b2d9c2fee6d6cd8
Yasin Kavak <ysnkvk2002@gmail.com>
2024-10-16 15:28:11 +0300

    reset
    reset

--- FormsApp/FormsApp/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FormsApp.Models
-{
-    public class Category
-    {
-        public int CategoryId { get; set; }
-        public string? Name { get; set; }
-    }
-}
--- efcoreApp/efcoreApp/Data/Kurs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace efcoreApp.Data
-{
-    public class Kurs
-    {
-        public int KursId { get; set; }
-        public string? Baslik { get; set; }
-    }
-}
